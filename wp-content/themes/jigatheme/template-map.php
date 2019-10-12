f92331d65f61cf827b9533d7f99c52bdde0e70c0
arnaud <arnaud.kirscher.student@gmail.com>
2019-10-12 19:48:54 +0200

    maj template map

--- wp-content/themes/jigatheme/template-map.php
+++ wp-content/themes/jigatheme/template-map.php
@@ -35,16 +35,12 @@ Template Name: Carte
         });
         
         map.on('load', function() {
-        // Add a new source from our GeoJSON data and set the
-        // 'cluster' option to true. GL-JS will add the point_count property to your source data.
         map.addSource("earthquakes", {
         type: "geojson",
-        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
         data: "<?php echo get_template_directory_uri(); ?>/points.json",
         cluster: true,
-        clusterMaxZoom: 14, // Max zoom to cluster points on
-        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+        clusterMaxZoom: 16,
+        clusterRadius: 50
         });
         
         map.addLayer({
@@ -53,11 +49,6 @@ Template Name: Carte
         source: "earthquakes",
         filter: ["has", "point_count"],
         paint: {
-        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-        // with three steps to implement three types of circles:
-        //   * Blue, 20px circles when point count is less than 100
-        //   * Yellow, 30px circles when point count is between 100 and 750
-        //   * Pink, 40px circles when point count is greater than or equal to 750
         "circle-color": [
         "step",
         ["get", "point_count"],
@@ -104,7 +95,6 @@ Template Name: Carte
         }
         });
         
-        // inspect a cluster on click
         map.on('click', 'clusters', function (e) {
         var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
         var clusterId = features[0].properties.cluster_id;
