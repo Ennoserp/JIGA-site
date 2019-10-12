ca68157e187f5810ef6412cd2f0a37d9488ea06b
arnaud <arnaud.kirscher.student@gmail.com>
2019-10-12 22:54:20 +0200

    maj page

---
+++ wp-content/themes/jigatheme/404.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+  <h1 class="h3 mb-0 text-gray-800">404</h1>
+</div>
+
+404 not found
+
+
+</div>
+  <!-- End of Main Content -->
+
+<?php
+
+get_footer();
---
+++ wp-content/themes/jigatheme/page.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+  <h1 class="h3 mb-0 text-gray-800"><?php echo get_the_title(); ?></h1>
+</div>
+
+<?php echo the_content(); ?>
+
+
+</div>
+  <!-- End of Main Content -->
+
+<?php
+
+get_footer();
