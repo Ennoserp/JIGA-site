6e8aad3af4dbb6cc703384a49ff260e7c2230dda
arnaud <arnaud.kirscher.student@gmail.com>
2019-10-12 19:30:30 +0200

    add template map

--- wp-content/themes/jigatheme/template-map.php
+++ wp-content/themes/jigatheme/template-map.php
@@ -2,3 +2,131 @@
 /*
 Template Name: Carte
 */
+
+?><!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Carte des poteaux</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1.0" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" rel="stylesheet" />
+        <script src='https://api.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.js'></script>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.css' rel='stylesheet' />
+        <style>
+            html, body{
+                height: 100%;
+            }
+        </style>
+    </head>
+    <body class="">
+
+        <!--<h1>Carte des lampadaires de la ville</h1>-->
+
+        <div id='map' style='width: 100%; height: 100%;'></div>
+
+        <script>
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYXJuYXVka3N0dWQiLCJhIjoiY2sxbm1xeHdiMGM3NzNjcWtzZHhuY3g0NCJ9.414u70WFpax7al_AuexSQQ';
+        var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/dark-v10',
+        center: [ 3.083156, 45.773540 ],
+        zoom: 19
+        });
+        
+        map.on('load', function() {
+        // Add a new source from our GeoJSON data and set the
+        // 'cluster' option to true. GL-JS will add the point_count property to your source data.
+        map.addSource("earthquakes", {
+        type: "geojson",
+        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+        data: "<?php echo get_template_directory_uri(); ?>/points.json",
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+        });
+        
+        map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "earthquakes",
+        filter: ["has", "point_count"],
+        paint: {
+        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+        // with three steps to implement three types of circles:
+        //   * Blue, 20px circles when point count is less than 100
+        //   * Yellow, 30px circles when point count is between 100 and 750
+        //   * Pink, 40px circles when point count is greater than or equal to 750
+        "circle-color": [
+        "step",
+        ["get", "point_count"],
+        "#51bbd6",
+        100,
+        "#f1f075",
+        750,
+        "#f28cb1"
+        ],
+        "circle-radius": [
+        "step",
+        ["get", "point_count"],
+        20,
+        100,
+        30,
+        750,
+        40
+        ]
+        }
+        });
+        
+        map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "earthquakes",
+        filter: ["has", "point_count"],
+        layout: {
+        "text-field": "{point_count_abbreviated}",
+        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+        "text-size": 12
+        }
+        });
+        
+        map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "earthquakes",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+        "circle-color": "#11b4da",
+        "circle-radius": 8 ,
+        "circle-stroke-width": 1,
+        "circle-stroke-color": "#fff"
+        }
+        });
+        
+        // inspect a cluster on click
+        map.on('click', 'clusters', function (e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
+        var clusterId = features[0].properties.cluster_id;
+        map.getSource('earthquakes').getClusterExpansionZoom(clusterId, function (err, zoom) {
+        if (err)
+        return;
+        
+        map.easeTo({
+        center: features[0].geometry.coordinates,
+        zoom: zoom
+        });
+        });
+        });
+        
+        map.on('mouseenter', 'clusters', function () {
+        map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', 'clusters', function () {
+        map.getCanvas().style.cursor = '';
+        });
+        });
+        </script>
+
+    </body>
+</html>
